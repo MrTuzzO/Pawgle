3124713361ce24cbe0d4bc64efabc91ff9517188
MrTuzzO <khirulislam5750@gmail.com>
2025-03-09 21:11:11 +0600

    favicon added

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,8 @@
+var link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/png';
+link.href = 'assets/img/favicon.png';
+document.head.appendChild(link);
+
 const root_api = "https://pet-adoption-backend-drf.vercel.app";
 // const root_api = "http://127.0.0.1:8000";
